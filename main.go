14209ea6a5e31dff95f0f3f2ef78ab96d92b66ec
Marko Mikulicic <mkm@bitnami.com>
2018-03-12 22:36:18 +0100

    firebase

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var (
+	listen = flag.String("listen", ":8080", "http addr:port")
+)
 
 func main() {
+	flag.Parse()
+
+	fs := http.FileServer(http.Dir("ui"))
+	http.Handle("/", fs)
+
+	log.Fatal(http.ListenAndServe(*listen, nil))
+
 	fmt.Println("ok")
 }
---
+++ ui/README.md
@@ -0,0 +1,8 @@
+```
+    apiKey: "AIzaSyBW_Zdgoekbgj8AyelJ3kQ-bVfU_2KTbao",
+    authDomain: "stacksmith-193010.firebaseapp.com",
+    databaseURL: "https://stacksmith-193010.firebaseio.com",
+    projectId: "stacksmith-193010",
+    storageBucket: "",
+    messagingSenderId: "1059334930734"
+```
