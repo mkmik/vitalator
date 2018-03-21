4eaa92433150dc63556143f8e969ad34c0caf7ad
Marko Mikulicic <mkm@bitnami.com>
2018-03-21 23:12:16 +0100

    Use symbols instead of strings

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,39 +1,47 @@
 import {Program, Cmd, none, run, htmlForMessage, onInput} from './elmets';
 const html = htmlForMessage<Msg>();
 
-type Msg = Inc | Dec | Rst | Changed; 
+type Msg = Msg.Inc | Msg.Dec | Msg.Rst | Msg.Changed;
+module Msg {
+  export const INC = Symbol("INC");
+  export interface Inc { kind: typeof INC }
 
-interface Inc { kind: "Inc"}
-interface Dec { kind: "Dec" }
-interface Rst { kind: "Rst" }
-interface Changed { kind: "Changed", value: string }
-function Changed(v: string): Changed { return { kind: "Changed", value: v }; }
+  export const DEC = Symbol("DEC");
+  export interface Dec { kind: typeof DEC }
+
+  export const RST = Symbol("RST");
+  export interface Rst { kind: typeof RST }
+
+  export const CHANGED = Symbol("CHANGED");
+  export interface Changed { kind: typeof CHANGED, value: string }
+  export function Changed(v: string): Changed { return { kind: CHANGED, value: v }; }
+}
 
 function update(msg: Msg, model: number): number {
   switch(msg.kind) {
-    case "Inc":
+    case Msg.INC:
       return model + 1;
-    case "Dec":
+    case Msg.DEC:
       return model - 1;
-    case "Rst":
+    case Msg.RST:
       return 0;
-    case "Changed":
+    case Msg.CHANGED:
       console.log("changed", msg);
       return model;
   }
 };
 
-let inner = html`<button on-click=${ { kind: "Rst" }}>reset</button>`;
+let inner = html`<button on-click=${ { kind: Msg.RST }}>reset</button>`;
 let footer = html`<hr><em>${inner}</em>`;
 let txt = "answer";
 
 let view = (s: number) => html`<p>
   The ${txt} is <b>${s}</b>
-  <button on-click=${ { kind: "Inc" } }>inc</button>
-  <button on-click=${ { kind: "Dec" } }>dec</button>
+  <button on-click=${ { kind: Msg.INC } }>inc</button>
+  <button on-click=${ { kind: Msg.DEC } }>dec</button>
   ${footer}
   <hr>
-  <input on-change=${ onInput(Changed) }></input>
+  <input on-change=${ onInput(Msg.Changed) }></input>
 </p>`;
 
 run(document.getElementById("root"), {
