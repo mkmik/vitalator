642e55c887073f847e0c3a9c114b145ecbc24c99
Marko Mikulicic <mkm@bitnami.com>
2018-03-22 01:31:04 +0100

    Rename to type

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -7,14 +7,14 @@ const RST = Symbol("RST");
 const CHANGED = Symbol("CHANGED");
 
 type Msg = Readonly< 
-  { kind: typeof INC } |
-  { kind: typeof DEC } |
-  { kind: typeof RST } |
-  { kind: typeof CHANGED} & ElementValue>;
+  { type: typeof INC } |
+  { type: typeof DEC } |
+  { type: typeof RST } |
+  { type: typeof CHANGED} & ElementValue>;
 
 function update(msg: Msg, model: number): number {
   console.log("updating", msg, "(stringified as", JSON.stringify(msg), ")");
-  switch(msg.kind) {
+  switch(msg.type) {
     case INC:
       return model + 1;
     case DEC:
@@ -22,21 +22,22 @@ function update(msg: Msg, model: number): number {
     case RST:
       return 0;
     case CHANGED:
+      console.log("todo something with entered text", msg.value);
       return model;
   }
 };
 
-let inner = html`<button on-click=${ { kind: RST }}>reset</button>`;
+let inner = html`<button on-click=${ { type: RST }}>reset</button>`;
 let footer = html`<hr><em>${inner}</em>`;
 let txt = "answer";
 
 let view = (s: number) => html`<p>
   The ${txt} is <b>${s}</b>
-  <button on-click=${ { kind: INC } }>inc</button>
-  <button on-click=${ { kind: DEC } }>dec</button>
+  <button on-click=${ { type: INC } }>inc</button>
+  <button on-click=${ { type: DEC } }>dec</button>
   ${footer}
   <hr>
-  <input on-change=${ { kind: CHANGED, ...new ElementValue()} }></input>
+  <input on-change=${ { type: CHANGED, ...new ElementValue()} }></input>
 </p>`;
 
 run(document.getElementById("root"), {
